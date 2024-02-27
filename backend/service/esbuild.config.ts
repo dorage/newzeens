a82c9c34ad6568d22936c754f41381728df3fe42
dorage-san <baloonflower554@gmail.com>
2024-02-27 19:56:52 +0900

    fix local-service
    - add log on kysely startup
    - remove better-sqlite3 from esbuild deps
    - add deps better-sqlite3 on local-service

--- backend/service/esbuild.config.ts
+++ backend/service/esbuild.config.ts
@@ -3,68 +3,69 @@ import type { BuildOptions, Plugin } from "esbuild";
 import path from "path";
 
 const productionResolvePlugins: Plugin = {
-  name: "replaceDev",
-  setup(build) {
-    // Redirect all paths starting with "images/" to "./public/images/"
-    build.onResolve({ filter: /.*\.dev/ }, (args) => {
-      return { path: path.join(process.cwd(), "src", "helpers", "mock.ts") };
-    });
-  },
+	name: "replaceDev",
+	setup(build) {
+		// Redirect all paths starting with "images/" to "./public/images/"
+		build.onResolve({ filter: /.*\.dev/ }, (args) => {
+			return { path: path.join(process.cwd(), "src", "helpers", "mock.ts") };
+		});
+	},
 };
 
 const buildOption: BuildOptions = {
-  platform: "node",
-  entryPoints: ["src/index.ts"],
-  loader: {
-    // ensures .node binaries are copied to ./dist
-    ".node": "copy",
-  },
-  bundle: true,
+	platform: "node",
+	entryPoints: ["src/index.ts"],
+	loader: {
+		// ensures .node binaries are copied to ./dist
+		".node": "copy",
+	},
+	bundle: true,
+	external: ["better-sqlite3"],
 };
 
 const devBuildOption: BuildOptions = {
-  ...buildOption,
-  outfile: "dist/index.cjs",
+	...buildOption,
+	outfile: "dist/index.cjs",
 };
 
 const prdBuildOption: BuildOptions = {
-  ...buildOption,
-  outfile: "dist/index.cjs",
-  plugins: [productionResolvePlugins],
-  minify: true,
+	...buildOption,
+	outfile: "dist/index.cjs",
+	plugins: [productionResolvePlugins],
+	minify: true,
 };
 
 function getBuildOption() {
-  // development option
-  if (process.argv.includes("--dev") || process.argv.includes("-d")) {
-    console.log("🛠️ DEV MODE");
-    return devBuildOption;
-  }
-  // production option
-  if (process.argv.includes("--production") || process.argv.includes("-p")) {
-    console.log("🚀 PRODUCTION MODE");
-    return prdBuildOption;
-  }
-  return devBuildOption;
+	// development option
+	if (process.argv.includes("--dev") || process.argv.includes("-d")) {
+		console.log("🛠️ DEV MODE");
+		return devBuildOption;
+	}
+	// production option
+	if (process.argv.includes("--production") || process.argv.includes("-p")) {
+		console.log("🚀 PRODUCTION MODE");
+		return prdBuildOption;
+	}
+	return devBuildOption;
 }
 
 async function main() {
-  const buildOption = getBuildOption();
+	const buildOption = getBuildOption();
 
-  // watchmode
-  if (process.argv.includes("--watch") || process.argv.includes("-w")) {
-    const ctx = await esbuild.context(buildOption);
-    await ctx.watch();
-    return;
-  }
+	// watchmode
+	if (process.argv.includes("--watch") || process.argv.includes("-w")) {
+		const ctx = await esbuild.context(buildOption);
+		await ctx.watch();
+		return;
+	}
 
-  return await esbuild
-    .build(buildOption)
-    .then((e) => {
-      console.log(e);
-      console.log(`[Result]: ${buildOption.outfile}`);
-    })
-    .catch((e) => console.error(e));
+	return await esbuild
+		.build(buildOption)
+		.then((e) => {
+			console.log(e);
+			console.log(`[Result]: ${buildOption.outfile}`);
+		})
+		.catch((e) => console.error(e));
 }
 
 main();
--- backend/service/src/libs/kysely.ts
+++ backend/service/src/libs/kysely.ts
@@ -1,4 +1,5 @@
 import { KyselyAdapter } from "kysely-schema";
 import path from "path";
 
+console.log(path.resolve(process.cwd(), "db"));
 export const Ky = KyselyAdapter(path.resolve(process.cwd(), "db"), { fileMustExist: true });
