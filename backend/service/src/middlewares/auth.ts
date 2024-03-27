637fd17a780be36a7cfe382c7ae431471fe2a9dd
dorage-san <baloonflower554@gmail.com>
2024-03-27 17:12:52 +0900

    remove private repo deps

--- backend/service/src/middlewares/auth.ts
+++ backend/service/src/middlewares/auth.ts
@@ -1,9 +1,7 @@
-import HonoAuth from "hono-auth";
 import { createMiddleware } from "hono/factory";
 
 export const useAuth = () => {
-  return createMiddleware(async (c, next) => {
-    c.set("", HonoAuth.useAuthHandler(c, false));
-    await next();
-  });
+	return createMiddleware(async (c, next) => {
+		await next();
+	});
 };
