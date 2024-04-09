88cc72f48cba428a2b55c48d0fd846c71f2c4632
dorage-san <baloonflower554@gmail.com>
2024-04-09 14:19:26 +0900

    style: reformatting

--- backend/service/src/app.ts
+++ backend/service/src/app.ts
@@ -10,7 +10,7 @@ import admin from "./routes/admin";
 import service from "./routes/services";
 
 declare module "hono" {
-	interface ContextVariableMap { }
+  interface ContextVariableMap {}
 }
 
 const app = new OpenAPIHono();
@@ -19,49 +19,49 @@ app.use("*", logger());
 
 // cors
 app.use(
-	"*",
-	cors({
-		origin: (origin) => origin,
-		credentials: true,
-	})
+  "*",
+  cors({
+    origin: (origin) => origin,
+    credentials: true,
+  })
 );
 
 app.onError(async (err, c) => {
-	console.error("[ERROR] ", c.req.method, c.req.path);
-	console.error("[Message]", err);
-	// console.error("Headers: ", c.req.header());
-	// console.error("Params: ", c.req.param());
-	// console.error("Json: ", await c.req.json());
-	if (err instanceof HTTPException) {
-		return c.text(`${err.name} ${err.message}`, err.status);
-	}
-	if (err instanceof ZodError) {
-		return c.text(err.toString(), 400);
-	}
-	return c.text("", 404);
+  console.error("[ERROR] ", c.req.method, c.req.path);
+  console.error("[Message]", err);
+  // console.error("Headers: ", c.req.header());
+  // console.error("Params: ", c.req.param());
+  // console.error("Json: ", await c.req.json());
+  if (err instanceof HTTPException) {
+    return c.text(`${err.name} ${err.message}`, err.status);
+  }
+  if (err instanceof ZodError) {
+    return c.text(err.toString(), 400);
+  }
+  return c.text("", 404);
 });
 
 app.get("/", async (c) => {
-	return c.json({ okay: true });
+  return c.json({ okay: true });
 });
 
 app.notFound(async (c) => {
-	return c.text("", 404);
+  return c.text("", 404);
 });
 
 // register a securtiy component, OpenAPI
 app.openAPIRegistry.registerComponent("securitySchemes", "Bearer", {
-	type: "http",
-	scheme: "bearer",
-	bearerFormat: "JWT",
+  type: "http",
+  scheme: "bearer",
+  bearerFormat: "JWT",
 });
 // The OpenAPI documentation will be available at /doc
 app.doc("/open-api", {
-	openapi: "3.0.0",
-	info: {
-		version: "1.1.0",
-		title: "Newzeens API",
-	},
+  openapi: "3.0.0",
+  info: {
+    version: "1.1.0",
+    title: "Newzeens API",
+  },
 });
 // SwaggerUI
 app.get("/doc", swaggerUI({ url: "/open-api" }));
