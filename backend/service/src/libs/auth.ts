e15b90ac52e6c9046be3a5b34391e37cc7fde38d
dorage-san <baloonflower554@gmail.com>
2024-04-09 15:05:03 +0900

    add: check invalid jti or not

--- backend/service/src/libs/auth.ts
+++ backend/service/src/libs/auth.ts
@@ -1,14 +1,20 @@
+import { z } from "@hono/zod-openapi";
+import crypto from "crypto";
 import { Context } from "hono";
 import { deleteCookie, getSignedCookie, setSignedCookie } from "hono/cookie";
 import { HTTPException } from "hono/http-exception";
 import * as jwt from "hono/jwt";
 import moment from "moment";
-import crypto from "crypto";
+import { Ky } from "./kysely";
+
+const zRefreshPayload = z.object({
+	jti: z.string().length(36),
+	exp: z.string(),
+});
 
 const issueTokens = async (c: Context) => {
 	const now = moment();
 	const payload = {
-		jti: crypto.randomUUID(),
 		// The token is checked to ensure it is not being used before a specified time.
 		nbf: now.toISOString(),
 		// The token is checked to ensure it is not issued in the future.
@@ -16,11 +22,18 @@ const issueTokens = async (c: Context) => {
 	};
 
 	const accessToken = await jwt.sign(
-		{ ...payload, exp: now.add(process.env.JWT_ACCESS_EXP, "ms").toISOString() },
+		{
+			...payload,
+			exp: now.add(process.env.JWT_ACCESS_EXP, "ms").toISOString(),
+		},
 		process.env.JWT_SECRET
 	);
 	const refreshToken = await jwt.sign(
-		{ ...payload, exp: now.add(process.env.JWT_REFRESH_EXP, "ms").toISOString() },
+		{
+			...payload,
+			jti: crypto.randomUUID(),
+			exp: now.add(process.env.JWT_REFRESH_EXP, "ms").toISOString(),
+		},
 		process.env.JWT_SECRET
 	);
 
@@ -52,7 +65,19 @@ const refresh = async (c: Context) => {
 	if (typeof refreshToken !== "string") {
 		throw new HTTPException(401, { message: "Unauthorized" });
 	}
-	await jwt.verify(refreshToken, process.env.JWT_SECRET);
+	const payload = zRefreshPayload.parse(await jwt.verify(refreshToken, process.env.JWT_SECRET));
+
+	// insert jti. if it is alredy exists, throw an error
+	try {
+		await Ky.insertInto("jtis")
+			.values({
+				jti: payload.jti,
+				expires_in: payload.exp,
+			})
+			.execute();
+	} catch (err) {
+		throw new HTTPException(401, { message: "Invalid refresh token" });
+	}
 
 	return issueTokens(c);
 };
