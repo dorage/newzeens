688808dd9b513aec26fa00d10b70843a9a3bae48
dorage-san <baloonflower554@gmail.com>
2024-04-12 21:26:19 +0900

    test(db): update jest module map

--- packages/kysely-schema/jest.config.js
+++ packages/kysely-schema/jest.config.js
@@ -1,6 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  setupFiles: ["<rootDir>/tests/setups/dotenv.ts"],
+	preset: "ts-jest",
+	testEnvironment: "node",
+	setupFiles: ["dotenv/config"],
+	moduleNameMapper: {
+		"^@/src/(.*)$": "<rootDir>/src/$1",
+		"^@/tests/(.*)$": "<rootDir>/tests/$1",
+	},
 };
