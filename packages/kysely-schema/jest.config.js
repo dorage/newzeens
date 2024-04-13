e57ab30fa844e4fc6bcbd84dacbdabae7ad99337
dorage-san <baloonflower554@gmail.com>
2024-04-13 19:13:40 +0900

    chore(db): add tsconfig/jest path

--- packages/kysely-schema/jest.config.js
+++ packages/kysely-schema/jest.config.js
@@ -1,10 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-	preset: "ts-jest",
-	testEnvironment: "node",
-	setupFiles: ["dotenv/config"],
-	moduleNameMapper: {
-		"^@/src/(.*)$": "<rootDir>/src/$1",
-		"^@/tests/(.*)$": "<rootDir>/tests/$1",
-	},
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFiles: ["dotenv/config"],
+  moduleNameMapper: {
+    "^@/src/(.*)$": "<rootDir>/src/$1",
+    "^@/tests/(.*)$": "<rootDir>/tests/$1",
+    "^@/migrator/(.*)$": "<rootDir>/migrator/$1",
+  },
 };
