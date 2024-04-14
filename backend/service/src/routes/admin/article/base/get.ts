55fcc80693e0fa1e270d17b66602b4651611fbc7
dorage-san <baloonflower554@gmail.com>
2024-04-15 00:24:02 +0900

    refact(be): seperate infra layer from service layer on '/admin/article/get'

--- backend/service/src/routes/admin/article/base/get.ts
+++ backend/service/src/routes/admin/article/base/get.ts
@@ -1,16 +1,15 @@
 import Tag from "@/src/constants/tags";
-import { Ky } from "@/src/libs/kysely";
 import { queryObject } from "@/src/openapi/query-objects";
 import OpenAPISchema from "@/src/openapi/schemas";
 import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
 import { ArticleSchema } from "kysely-schema";
+import { retreiveArticles } from "./get.query";
 
 export const zQuery = z.object({
   page: queryObject.paginationPage(),
   limit: queryObject.paginationLimit(),
   name: ArticleSchema.shape.title.optional().default("").openapi({
     description: "검색할 제목입니다",
-    example: "제목",
   }),
   publisher_id: ArticleSchema.shape.publisher_id.optional().openapi({
     description: "publisher의 id로 관련 article을 불러옵니다",
@@ -53,26 +52,9 @@ export type EndpointType = typeof ep;
 export const ep = app.openapi(route, async (c) => {
   const query = zQuery.parse(c.req.query());
 
-  // keyword 에 따라
-  // keyword + search term에 따라
-  // is_enalbed 에 따라
-  // page size / limit 에 따라
+  const articles = await retreiveArticles(query);
 
-  const keywordGroups = await Ky.selectFrom("articles")
-    .selectAll()
-    .where((eb) => {
-      const conditions = [];
-      if (query.name) conditions.push(eb("title", "like", `%${query.name}%`));
-      if (query.is_enabled) conditions.push(eb("is_enabled", "=", query.is_enabled));
-      if (query.publisher_id) conditions.push(eb("publisher_id", "=", query.publisher_id));
-      return eb.and(conditions);
-    })
-    .limit(query.limit)
-    .offset(query.page * query.limit)
-    .orderBy("created_at", "desc")
-    .execute();
-
-  return c.json(zRes.parse(keywordGroups));
+  return c.json(zRes.parse(articles));
 });
 
 export default app;
