b6c15298a1b538d32254d96afbfb3570678edcdc
dorage-san <baloonflower554@gmail.com>
2024-04-20 21:08:38 +0900

    fix(db): datetime column zodtype

--- packages/kysely-schema/src/schemas/columns/date.ts
+++ packages/kysely-schema/src/schemas/columns/date.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 import moment from "moment";
 
-export const zMomentDatetime = z
-  .string()
-  .transform((arg) => moment(arg, "YYYY-MM-DD HH:mm:ss").utc(false));
+// export const zMomentDatetime = z
+//   .string()
+//   .transform((arg) => moment(arg, "YYYY-MM-DD HH:mm:ss").utc(false));
+
+export const zMomentDatetime = z.string().datetime();
