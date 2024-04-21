645363fa9ad86abaabdf14d753d16b7031c8c7ef
dorage-san <baloonflower554@gmail.com>
2024-04-22 00:03:18 +0900

    test(be): query & controller test

--- backend/service/src/routes/services/publisher/[id]/get.test.ts
+++ backend/service/src/routes/services/publisher/[id]/get.test.ts
@@ -1,32 +1,66 @@
 import { Ky } from "@/src/libs/kysely";
-import { getPublisherSpec, getRelatedPublishers } from "./get.query";
-import { controller } from "./get.controller";
+import { sql } from "kysely";
 import { zRes } from "./get";
+import { controller } from "./get.controller";
+import { getRecentArticleOfPublisher, getRelatedPublishers } from "./get.query";
+import moment from "moment";
+
+// TODO; some mock data is out of the constraint, it should be fixed to pass tests
+
+const getRandomPublihser = () => {
+  return Ky.selectFrom("publishers")
+    .selectAll()
+    .where("is_enabled", "=", sql`TRUE`)
+    .limit(1)
+    .orderBy(sql`RANDOM()`)
+    .executeTakeFirstOrThrow();
+};
 
 describe("get.query", () => {
-  test("publisher load well", async () => {
-    const publisher = await Ky.selectFrom("publishers")
-      .selectAll()
-      .limit(1)
-      .executeTakeFirstOrThrow();
-    const publisherId = publisher.id;
+  test("the result of selected related publisher sould be parsed successfully", async () => {
+    const publisher = await getRandomPublihser();
 
-    const promises = [getPublisherSpec({ publisherId }), getRelatedPublishers({ publisherId })];
+    const res = await getRelatedPublishers({ publisherId: publisher.id });
 
-    const [pub, relPub] = await Promise.all(promises);
-    console.log(pub, relPub);
+    expect(zRes.shape.related_publishers.safeParse(res).success).toEqual(true);
+  });
+
+  test("the result of selected related publisher should be different each time", async () => {
+    const publisher = await getRandomPublihser();
+
+    const resA = await getRelatedPublishers({ publisherId: publisher.id });
+    const resB = await getRelatedPublishers({ publisherId: publisher.id });
+
+    expect(resA.every((a) => resB.some((b) => a.id === b.id))).toEqual(false);
+  });
+
+  test("the result of selected recent article shold be parsed successfully", async () => {
+    const publisher = await getRandomPublihser();
+
+    const res = await getRecentArticleOfPublisher({ publisherId: publisher.id });
+
+    expect(zRes.shape.recent_articles.safeParse(res).success).toEqual(true);
+  });
+
+  test("the result of selected recent articles should be sorted descending", async () => {
+    const publisher = await getRandomPublihser();
+
+    const res = await getRecentArticleOfPublisher({ publisherId: publisher.id });
+    const sorted = [...res];
+    sorted.sort((a, b) => Number(moment(b.created_at)) - Number(moment(a.created_at)));
+
+    expect(res.every((_, idx) => res[idx].id === sorted[idx].id)).toEqual(true);
   });
 });
 
 describe("get.controller", () => {
-  it("should be success", async () => {
+  it("the result of the controller should be parsed successfully", async () => {
     const publisher = await Ky.selectFrom("publishers")
       .selectAll()
       .limit(1)
       .executeTakeFirstOrThrow();
 
     const res = await controller({ publisherId: publisher.id });
-    console.log(res);
     expect(zRes.safeParse(res).success).toEqual(true);
   });
 });
