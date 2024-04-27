6d9dd0673ab1cf02253ebe8885bfdbbbc29f6220
dorage-san <baloonflower554@gmail.com>
2024-04-27 21:42:52 +0900

    test(be): add query tests on /rank

--- backend/service/src/routes/services/rank/base/get.test.ts
+++ backend/service/src/routes/services/rank/base/get.test.ts
@@ -1,5 +1,22 @@
+import { Ky } from "@/src/libs/kysely";
 import { zRes } from "./get";
-import { getPublisherRank } from "./get.query";
+import { getKeyword, getPublisherRank } from "./get.model";
+import { sql } from "kysely";
+
+const getRandomKeyowrd = async () => {
+  const keyword = await Ky.selectFrom("keywords")
+    .where((qb) =>
+      qb.eb(
+        "keywords.keyword_group_id",
+        "=",
+        qb.selectFrom("keyword_groups").select(["id"]).where("name", "=", "분야").limit(1)
+      )
+    )
+    .selectAll()
+    .orderBy(sql`RANDOM()`)
+    .executeTakeFirstOrThrow();
+  return keyword;
+};
 
 describe("get.query", () => {
   test("the result of select publisher rank should be parsed successfully", async () => {
@@ -21,5 +38,43 @@ describe("get.query", () => {
       expect(page.every((_, idx) => base[baseIdx + idx].id === page[idx].id)).toEqual(true);
       lastPublisherId = page[page.length - 1].id;
     }
+  });
+
+  test("the result of select publisher rank with wrong cursor should equal with no cursor", async () => {
+    const withNoCursor = await getPublisherRank({ limit: 10 });
+    const withWrongCursor = await getPublisherRank({
+      limit: 10,
+      lastPublisherId: "💩💩💩💩💩💩💩💩",
+    });
+
+    expect(withWrongCursor).toEqual(withNoCursor);
+  });
+
+  test("the result of select keyword should return same row", async () => {
+    const keyword = await getRandomKeyowrd();
+
+    const result = await getKeyword({ keyword: keyword.name });
+
+    expect(result).toEqual(keyword);
+  });
+
+  test("the result of select publisher rank with keyword filter should have same keywords", async () => {
+    const keyword = await getRandomKeyowrd();
+
+    const result = await getPublisherRank({ limit: 10, keyword: keyword.name });
+
+    expect(result.every((_, idx) => keyword.name === result[idx].keywords[0].keyword_name)).toEqual(
+      true
+    );
+  });
+
+  test("the result of select publisher rank with wrong keyword should equal with no filter", async () => {
+    const withNoKeyword = await getPublisherRank({ limit: 10 });
+    const withWrongKeyword = await getPublisherRank({
+      limit: 10,
+      keyword: "💩💩💩💩💩💩💩💩",
+    });
+
+    expect(withWrongKeyword).toEqual(withNoKeyword);
   });
 });
