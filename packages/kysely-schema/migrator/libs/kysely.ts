ed0171eb773c73fdaec4ea0cef0927c252bc0d65
dorage-san <baloonflower554@gmail.com>
2024-04-29 16:30:27 +0900

    add: kysely-migration table schema to check last applied version name

--- packages/kysely-schema/migrator/libs/kysely.ts
+++ packages/kysely-schema/migrator/libs/kysely.ts
@@ -1,5 +1,9 @@
-import { KyselyAdapter } from "@/src/index";
+import { DB, KyselyAdapter } from "@/src/index";
+import { Kysely } from "kysely";
 import path from "path";
+import { KyKyselyMigrationTable } from "../schemas/kysely-migration";
 
 const db = process.env.DB_NAME;
-export const Ky = KyselyAdapter(path.resolve(db), { fileMustExist: true });
+export const Ky = KyselyAdapter(path.resolve(db), { fileMustExist: true }) as Kysely<
+  DB & { kysely_migration: KyKyselyMigrationTable }
+>;
