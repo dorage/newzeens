cb63d665238c17d577759143ae070256aa7015e1
dorage-san <baloonflower554@gmail.com>
2024-04-29 17:57:48 +0900

    test: add roots field in jest.config & update test scripts

--- packages/kysely-schema/jest.config.ts
+++ packages/kysely-schema/jest.config.ts
@@ -3,6 +3,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFiles: ["dotenv/config", "<rootDir>/tests/setups/fast-check.ts"],
+  roots: ["<rootDir>/src/migrations/v0.0.0-2024-04-29T07:31:56.202Z/"],
   moduleNameMapper: {
     "^@/src/(.*)$": "<rootDir>/src/$1",
     "^@/tests/(.*)$": "<rootDir>/tests/$1",
