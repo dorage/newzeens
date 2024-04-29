cdfcdd357a580aab5b7e4881d4a84733a534a9ba
dorage-san <baloonflower554@gmail.com>
2024-04-29 13:46:54 +0900

    fix: rename/error in const

--- packages/kysely-schema/migrator/libs/new-migration.ts
+++ packages/kysely-schema/migrator/libs/new-migration.ts
@@ -35,7 +35,7 @@ export const VersionSchema = _VersionSchema;
 
 const CONTENT_MIGRATION = (newMigrationName: string) => `
 import { Kysely, sql } from 'kysely'
-import { DB } from "./${newMigrationName}";
+import type { DB } from "./${newMigrationName}/index";
 
 /* 
 */
@@ -52,27 +52,31 @@ const createNewMigrationSchema = (newMigrationName: string) => {
   fs.writeFileSync(path.join(DIR_MIGRATION, newMigrationName, "index.ts"), CONTENT_DEFAULT_DB, {
     encoding: "utf8",
   });
-  fs.mkdirSync(path.join(DIR_MIGRATION, newMigrationName, "schema"));
-  fs.writeFileSync(
-    path.join(DIR_MIGRATION, newMigrationName, "schemas", "versions.ts"),
-    CONTENT_DEFAULT_SCHEMA,
-    { encoding: "utf8" }
-  );
+
+  const DIR_SCHEMA = path.join(DIR_MIGRATION, newMigrationName, "schemas");
+  fs.mkdirSync(DIR_SCHEMA);
+  fs.writeFileSync(path.join(DIR_SCHEMA, "versions.ts"), CONTENT_DEFAULT_SCHEMA, {
+    encoding: "utf8",
+  });
 };
 
 const copyLastestMigrationSchema = (newMigrationName: string) => {
   const latestMigrationName = getLatestMigrationName();
 
   // IF it is the first migration file
-  // OR there has no {newMigrationName} folder to copy
-  if (latestMigrationName == null || fs.existsSync(path.join(DIR_MIGRATION, latestMigrationName))) {
+  // OR {newMigrationName} folder is not exist
+  if (
+    latestMigrationName == null ||
+    !fs.existsSync(path.join(DIR_MIGRATION, latestMigrationName))
+  ) {
     createNewMigrationSchema(newMigrationName);
     return;
   }
 
   fs.cpSync(
     path.join(DIR_MIGRATION, latestMigrationName),
-    path.join(DIR_MIGRATION, newMigrationName)
+    path.join(DIR_MIGRATION, newMigrationName),
+    { recursive: true }
   );
 };
 
@@ -81,12 +85,12 @@ const getLatestMigrationName = (): string | null => {
   ls.sort();
   const lastMigration = ls[ls.length - 1];
 
-  return lastMigration ?? null;
+  return lastMigration.split(".ts").shift() ?? null;
 };
 
-const getNewMigrationName = (version: string): string => {
+const getNewMigrationName = (prefix: string): string => {
   const now = moment().utc(false).toISOString(false);
-  return `${version}-${now}`;
+  return `${prefix}-${now}`;
 };
 
 const createNewMigrationFile = (newMigrationName: string) => {
@@ -99,8 +103,8 @@ const createNewMigrationFile = (newMigrationName: string) => {
   );
 };
 
-export const createNewMigration = (version = "0.0.0") => {
-  const newMigrationName = getNewMigrationName(version);
+export const createNewMigration = (prefix = "v0.0.0") => {
+  const newMigrationName = getNewMigrationName(prefix);
 
   /*
    * migrations/
@@ -108,8 +112,8 @@ export const createNewMigration = (version = "0.0.0") => {
    * │  ├─ {table}.ts          -- Kysely table interface & Zod schema
    * ├─ {newMigrationName}.ts  -- Migration file, be consume by Kysely migrator
    * */
-  createNewMigrationFile(newMigrationName);
   copyLastestMigrationSchema(newMigrationName);
+  createNewMigrationFile(newMigrationName);
 
   console.log(`New Migration had generated: ${newMigrationName}`);
 };
