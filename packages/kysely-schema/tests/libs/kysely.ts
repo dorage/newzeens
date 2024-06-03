a2e4aab595ed3b6c08fe74707025820bede2bd32
dorage-san <baloonflower554@gmail.com>
2024-06-03 18:53:42 +0900

    add: db path in env.production

--- packages/kysely-schema/tests/libs/kysely.ts
+++ packages/kysely-schema/tests/libs/kysely.ts
@@ -2,8 +2,10 @@ import { KyselyAdapter } from "@/src/index";
 import { sql } from "kysely";
 import path from "path";
 
-const db = process.env.DB_NAME;
-export const Ky = KyselyAdapter(path.resolve(db), { fileMustExist: true });
+const dbPath =
+  process.env.NODE_ENV === "production" ? process.env.DB_PATH : path.resolve(process.env.DB_NAME);
+
+export const Ky = KyselyAdapter(dbPath, { fileMustExist: true });
 
 export const testingTransaction = () => {
   beforeAll(async () => {
