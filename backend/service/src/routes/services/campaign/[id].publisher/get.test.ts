0bd7a1ef37e89cf14dcd9f4d1112aaa894e20737
dorage <baloonflower554@gmail.com>
2024-07-31 00:26:39 +0900

    fix: increase readability with mutliple queries

--- backend/service/src/routes/services/campaign/[id].publisher/get.test.ts
+++ backend/service/src/routes/services/campaign/[id].publisher/get.test.ts
@@ -1,15 +1,15 @@
 import { zRes } from "./get";
 import { controller } from "./get.controller";
-import { getCampaignPublisher } from "./get.model";
+import { getCampaignPublisher, getPublishersWithKeywords } from "./get.model";
 
-describe("GET /campaign/:id/article", () => {
-  test("the result of the campaign articles should return well-formatted json", async () => {
+describe("GET /campaign/:id/publisher", () => {
+  test("the result of the campaign publishers should return well-formatted json", async () => {
     const campaignArticles = await getCampaignPublisher({ campaignId: 2, limit: 4 });
 
     expect(zRes.safeParse(campaignArticles).success).toEqual(true);
   });
 
-  test("the result of the campaign articles should include length of articles by limit query", async () => {
+  test("the result of the campaign publishers should include length of publishers by limit query", async () => {
     const limit = 1;
     const campaignArticles = await getCampaignPublisher({ campaignId: 2, limit });
 
@@ -17,7 +17,7 @@ describe("GET /campaign/:id/article", () => {
   });
 });
 
-describe("GET /campaign/:id/article", () => {
+describe("GET /campaign/:id/publisher", () => {
   test("the result of controller should return well-formatted json", async () => {
     const campaignArticles = await controller({ campaignId: 1, limit: 4 });
 
