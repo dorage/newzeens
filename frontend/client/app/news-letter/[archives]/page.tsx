c1236f97ed76f5bc386c11e48572d4cbd7bd3aa1
lnuvy <lnuvy.code@gmail.com>
2024-08-13 21:39:36 +0900

    fix: 웹 클라이언트 빌드에러 수정 / 이제 또 build 시간 오래걸리는중

--- frontend/client/app/news-letter/[archives]/page.tsx
+++ frontend/client/app/news-letter/[archives]/page.tsx
@@ -1,16 +1,18 @@
 import React from "react"
+
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query"
-import DescriptionLayer from "./components/description-layer"
-import LastArticles from "./components/last-articles"
-import NewsLetterTitle from "./components/news-letter-title"
-import SuggestNewsLetters from "./components/suggest-newsletters"
-import TabletFixedButton from "./components/tablet-fixed-button"
+
 import detailQueryKey from "@/app/_apis/_query-key/detail"
 import detailApi from "@/app/_apis/detail-page"
 import Header from "@/app/_components/header"
 import { IdContextProvider } from "@/app/_context/id-context"
 import { NextPageProps } from "@/app/_types/next"
 import getQueryClient from "@/app/_utils/query-client"
+import DescriptionLayer from "./components/description-layer"
+import LastArticles from "./components/last-articles"
+import NewsLetterTitle from "./components/news-letter-title"
+import SuggestNewsLetters from "./components/suggest-newsletters"
+import TabletFixedButton from "./components/tablet-fixed-button"
 
 interface ArchivesPageParams {
   archives: string
