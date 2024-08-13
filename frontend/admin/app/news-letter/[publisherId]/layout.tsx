c3d6fb041cd8c5ce714cbe47108e45e8585cfc8d
lnuvy <lnuvy.code@gmail.com>
2024-08-13 20:33:09 +0900

    fix: admin build error

--- frontend/admin/app/news-letter/[publisherId]/layout.tsx
+++ frontend/admin/app/news-letter/[publisherId]/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import WidthWrapper from "@/app/_components/layout/width-wrapper"
 
-export default async function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode; articleList: any; keywordForm: any }) {
   return <WidthWrapper>{children}</WidthWrapper>
 }
