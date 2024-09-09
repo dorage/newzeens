590b4eecaf23701024be8f6b1da7548ce3ea6705
dorage <baloonflower554@gmail.com>
2024-09-09 18:40:51 +0900

    fix: route should not start with slash when use prefixURL

--- backend/worker/src/apis/admin.ts
+++ backend/worker/src/apis/admin.ts
@@ -8,7 +8,7 @@ const client = ky.create({ prefixUrl: HOST });
 
 const signIn = async () => {
   if (accessToken != null) return;
-  const res = await client.post(`/admin/auth/login`, {
+  const res = await client.post(`admin/auth/login`, {
     json: {
       id: "admin",
       password: "password",
@@ -22,7 +22,7 @@ const signIn = async () => {
 
 const getScrap = async (url: string) => {
   await signIn();
-  const res = await client.get(`/admin/scrap?url=${encodeURI(url)}`, {
+  const res = await client.get(`admin/scrap?url=${encodeURI(url)}`, {
     headers: { Authorization: `Bearer ${accessToken}` },
   });
   const json = (await res.json()) as { ok: boolean };
@@ -31,7 +31,7 @@ const getScrap = async (url: string) => {
 
 const postScrap = async (url: string) => {
   await signIn();
-  await ky.post("/admin/scrap", {
+  await client.post("/admin/scrap", {
     headers: { Authorization: `Bearer ${accessToken}` },
     json: { url },
   });
@@ -39,14 +39,15 @@ const postScrap = async (url: string) => {
 
 const getPublisherList = async () => {
   await signIn();
-  const res = await ky.get("/admin/publisher?page=0&limit=1000", {
+  const res = await client.get("admin/publisher?page=0&limit=1000", {
     headers: { Authorization: `Bearer ${accessToken}` },
   });
   const json = await res.json();
 
-  fs.writeFileSync(`publisher_list_${process.env.NODE_ENV}.json`, JSON.stringify(json, null, 2), {
+  fs.writeFileSync(`./publisher_list_${process.env.NODE_ENV}.json`, JSON.stringify(json, null, 2), {
     encoding: "utf-8",
   });
+  return true;
 };
 
 export const Admin = {
