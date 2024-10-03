e9d11c35ac5c8f0776e2ab001ba41e4632b3299f
lnuvy <lnuvy.code@gmail.com>
2024-10-03 16:49:23 +0900

    배너 스와이퍼

--- frontend/client/app/_components/home/main-banner/index.tsx
+++ frontend/client/app/_components/home/main-banner/index.tsx
@@ -1,12 +1,62 @@
 "use client"
 
-import React, { useEffect } from "react"
+import React from "react"
+import { Autoplay, Pagination } from "swiper/modules"
+import { Swiper, SwiperSlide } from "swiper/react"
+import DeveloperBanner from "@/public/banner/developer.png"
+import MarketerBanner from "@/public/banner/marketer.png"
+import DesignerBanner from "@/public/banner/designer.png"
+import Image from "next/image"
+import Link from "next/link"
+import { SwiperOptions } from "swiper/types"
+import "swiper/css"
+import "swiper/css/pagination"
 
-interface MainBannerProps {}
-
-const MainBanner = (props: MainBannerProps) => {
-  const {} = props
-  return <div className="h-237 bg-bg-2 flex w-full items-center justify-center">배너영역</div>
+const MainBanner = () => {
+  return (
+    <div className="max-w-[1280px] mx-auto">
+      <Swiper {...BANNER_SWIPER_SETTINGS}>
+        {BANNER_DATA.map((banner) => (
+          <SwiperSlide key={banner.id}>
+            <div className="w-full h-auto">
+              <Link href={banner.href}>
+                <Image src={banner.src} alt={banner.category} />
+              </Link>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  )
 }
 
 export default MainBanner
+
+const BANNER_SWIPER_SETTINGS: SwiperOptions = {
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  modules: [Pagination, Autoplay],
+}
+
+const BANNER_DATA = [
+  {
+    id: 1,
+    category: "developer",
+    src: DeveloperBanner,
+    href: "/search?word=IT",
+  },
+  {
+    id: 2,
+    category: "marketer",
+    src: MarketerBanner,
+    href: "/search?word=IT",
+  },
+  {
+    id: 3,
+    category: "designer",
+    src: DesignerBanner,
+    href: "/search?word=IT",
+  },
+]
