217256da4136c59cf2ca834366f315ba36d8ce92
dorage <baloonflower554@gmail.com>
2024-10-03 21:31:41 +0900

    fix: remove limit

--- backend/service/src/routes/services/search/base/get.test.ts
+++ backend/service/src/routes/services/search/base/get.test.ts
@@ -3,7 +3,7 @@ import { getPublishersWithSearchTerm } from "./get.model";
 
 describe("GET /campaign/:id/publisher", () => {
   test("the result of the campaign publishers should return well-formatted json", async () => {
-    const publishers = await getPublishersWithSearchTerm({ term: "레터", limit: 99 });
+    const publishers = await getPublishersWithSearchTerm({ term: "레터" });
 
     console.log(JSON.stringify(publishers, undefined, 2));
     expect(zRes.safeParse(publishers).success).toEqual(true);
