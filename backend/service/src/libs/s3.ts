42f4f09c0f84b9ba8a8a5c5c884931fce1e39658
dorage <baloonflower554@gmail.com>
2024-10-04 20:43:12 +0900

    ref: style

--- backend/service/src/libs/s3.ts
+++ backend/service/src/libs/s3.ts
@@ -1,54 +1,54 @@
 import {
-	GetObjectCommand,
-	PutObjectCommand,
-	PutObjectCommandInput,
-	S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+  PutObjectCommandInput,
+  S3Client,
 } from "@aws-sdk/client-s3";
 
 const s3Client = () =>
-	new S3Client({
-		region: process.env.R2_REGION,
-		endpoint: process.env.R2_ENDPOINT,
-		credentials: {
-			accessKeyId: process.env.R2_ACCESS_KEY_ID,
-			secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
-		},
-	});
+  new S3Client({
+    region: process.env.R2_REGION,
+    endpoint: process.env.R2_ENDPOINT,
+    credentials: {
+      accessKeyId: process.env.R2_ACCESS_KEY_ID,
+      secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+    },
+  });
 
 const getImageUrlFromKey = (key: string) => {
-	return `${process.env.ORIGIN_IMAGE}/${process.env.R2_BUCKET}/${key}`;
+  return `${process.env.ORIGIN_IMAGE}/${process.env.R2_BUCKET}/${key}`;
 };
 
 const getKeyFromImageUrl = (url: string) => {
-	return url.replace(`${process.env.ORIGIN_IMAGE}/${process.env.R2_BUCKET}/`, "");
+  return url.replace(`${process.env.ORIGIN_IMAGE}/${process.env.R2_BUCKET}/`, "");
 };
 
 export const uploadObject = async (input: {
-	Body: Exclude<PutObjectCommandInput["Body"], undefined>;
-	Key: Exclude<PutObjectCommandInput["Key"], undefined>;
+  Body: Exclude<PutObjectCommandInput["Body"], undefined>;
+  Key: Exclude<PutObjectCommandInput["Key"], undefined>;
 }) => {
-	const command = new PutObjectCommand({
-		Body: input.Body,
-		Bucket: process.env.R2_BUCKET,
-		Key: input.Key,
-	});
+  const command = new PutObjectCommand({
+    Body: input.Body,
+    Bucket: process.env.R2_BUCKET,
+    Key: input.Key,
+  });
 
-	await s3Client().send(command);
+  await s3Client().send(command);
 
-	return getImageUrlFromKey(input.Key);
+  return getImageUrlFromKey(input.Key);
 };
 
 export const getObject = async (url: string) => {
-	const key = getKeyFromImageUrl(url);
+  const key = getKeyFromImageUrl(url);
 
-	const command = new GetObjectCommand({
-		Bucket: process.env.R2_BUCKET,
-		Key: key,
-	});
+  const command = new GetObjectCommand({
+    Bucket: process.env.R2_BUCKET,
+    Key: key,
+  });
 
-	const result = await s3Client().send(command);
+  const result = await s3Client().send(command);
 
-	if (result.Body == null) throw new Error("No body");
+  if (result.Body == null) throw new Error("No body");
 
-	return result.Body.transformToByteArray();
+  return result.Body.transformToByteArray();
 };
