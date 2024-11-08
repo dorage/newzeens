1e217d305d45acd0cfe3a7bcf5ce314f18189eb2
lnuvy <lnuvy.code@gmail.com>
2024-11-08 12:33:42 +0900

    뉴스레터 sitemap

--- frontend/client/app/news-letter/[archives]/sitemap.ts
+++ frontend/client/app/news-letter/[archives]/sitemap.ts
@@ -4,21 +4,24 @@ const DOMAIN = "https://www.maillist.site"
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: DOMAIN,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
-    },
-    {
-      url: DOMAIN + "/ranking",
+      // 요즘IT
+      url: DOMAIN + "/news-letter/odsljy",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.7,
     },
     {
-      url: DOMAIN + "/search",
+      // 와이즈앱리테일굿즈
+      url: DOMAIN + "/news-letter/sgpktm",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      // 디자인나침반
+      url: DOMAIN + "/news-letter/bbzlfu",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
       priority: 0.7,
     },
   ]
